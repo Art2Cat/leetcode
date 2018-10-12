8b292a14a6cc1561d4b8173f9b3c82ea1a1098bd
rorschach.h <yiming.whz@gmail.com>
2018-10-12 10:29:03 +0800

    complete 507. Perfect Number

---
+++ 507. Perfect Number/solution.c
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool checkPerfectNumber(int num) {
+
+  int j = 0;
+  for (int i = num / 2; i > 0; i = i / 2) {
+    if (num % i == 0) {
+      j += i;
+    }
+    if (j == num) {
+      return true;
+    }
+    if (i != 1 && i % 2 != 0) {
+      i += 1;
+    }
+  }
+  return false;
+}
+
+int main() {
+  int n = 28;
+  if (checkPerfectNumber(n)) {
+    printf("%d is perfect number.\n", n);
+  }
+  int n1 = 99999999;
+  if (checkPerfectNumber(n1)) {
+    printf("%d is perfect number.\n", n1);
+  }
+}
